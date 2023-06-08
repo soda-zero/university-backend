794f300124298e530c755697977a75587760c213
zeroCalSoda <diego.ezequiel.scardulla@gmail.com>
2023-06-08 04:25:01 -0300

    Added career_level repository and handler

---
+++ private/db/career_level_repository.go
@@ -0,0 +1,115 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"zeroCalSoda/university-backend/private/db/models"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type CareerLevelRepository struct {
+	pool *pgxpool.Pool
+}
+
+func (r *CareerLevelRepository) Close() {
+	r.pool.Close()
+}
+
+func NewCareerLevelRepository() (*CareerLevelRepository, error) {
+	pool, err := ConnectPg()
+	if err != nil {
+		return nil, fmt.Errorf("Unable to connect to the database: %v", err)
+	}
+
+	return &CareerLevelRepository{
+		pool: pool,
+	}, nil
+}
+
+func (r *CareerLevelRepository) GetCareerLevels() ([]models.CareerLevel, error) {
+	rows, err := r.pool.Query(context.Background(), "SELECT id, name FROM career_level")
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			return nil, fmt.Errorf("Failed to fetch career_level: %w", pgErr)
+		}
+		return nil, fmt.Errorf("Failed to fetch career_level: %w", err)
+	}
+	defer rows.Close()
+
+	careerLevels := make([]models.CareerLevel, 0)
+	for rows.Next() {
+		career := models.CareerLevel{}
+		if err := rows.Scan(&career.ID, &career.Name); err != nil {
+			var pgErr *pgconn.PgError
+			if errors.As(err, &pgErr) {
+				return nil, fmt.Errorf("Failed to scan career_level data: %w", pgErr)
+			}
+			return nil, fmt.Errorf("Failed to scan career_level data: %w", err)
+		}
+		careerLevels = append(careerLevels, career)
+
+		if err := rows.Err(); err != nil {
+			var pgErr *pgconn.PgError
+			if errors.As(err, &pgErr) {
+				return nil, fmt.Errorf("Error occurred while fetching career_level: %w", pgErr)
+			}
+			return nil, fmt.Errorf("Error occurred while fetching career_level: %w", err)
+		}
+	}
+	return careerLevels, nil
+}
+
+func (r *CareerLevelRepository) GetCareerLevelByID(careerLevelID string) (models.CareerLevel, error) {
+	var careerLevel models.CareerLevel
+	err := r.pool.QueryRow(context.Background(), "SELECT id, name FROM career_level WHERE id = $1", careerLevelID).Scan(&careerLevel.ID, &careerLevel.Name)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			return models.CareerLevel{}, fmt.Errorf("Error occurred while fetching career_level: %w", pgErr)
+		}
+		return models.CareerLevel{}, fmt.Errorf("Error occurred while fetching career_level: %w", err)
+	}
+
+	return careerLevel, nil
+}
+
+func (r *CareerLevelRepository) CreateCareerLevel(name string) error {
+	_, err := r.pool.Exec(context.Background(), "INSERT INTO career_level(name) VALUES($1)", name)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			return fmt.Errorf("Error occurred while inserting career_level: %w", pgErr)
+		}
+		return fmt.Errorf("Error occurred while inserting career_level: %w", err)
+	}
+	return nil
+}
+
+func (r *CareerLevelRepository) DeleteCareerLevel(careerID string) error {
+	_, err := r.pool.Exec(context.Background(), "DELETE FROM career_level WHERE id = $1", careerID)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			return fmt.Errorf("Error occurred while deleting career_level: %w", pgErr)
+		}
+		return fmt.Errorf("Error occurred while deleting career_level: %w", err)
+	}
+	return nil
+}
+
+func (r *CareerLevelRepository) UpdateCareerLevel(careerLevelID string, newName string) error {
+	query := "UPDATE career_level SET name = $1 WHERE id = $2"
+	_, err := r.pool.Exec(context.Background(), query, newName, careerLevelID)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			return fmt.Errorf("Error occurred while updating career_level: %w", pgErr)
+		}
+		return fmt.Errorf("Error occurred while updating career_level: %w", err)
+	}
+	return nil
+}
---
+++ private/handlers/career_level_handler.go
@@ -0,0 +1,125 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"zeroCalSoda/university-backend/private/db"
+	"zeroCalSoda/university-backend/private/db/models"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func GetCareerLevels(w http.ResponseWriter, r *http.Request) {
+	repo, err := db.NewCareerLevelRepository()
+	if err != nil {
+		http.Error(w, "Failed to connect to the database", http.StatusInternalServerError)
+		return
+	}
+	defer repo.Close()
+
+	careerLevels, err := repo.GetCareerLevels()
+	if err != nil {
+		http.Error(w, "Failed to fetch career career levels", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err = json.NewEncoder(w).Encode(careerLevels)
+	if err != nil {
+		http.Error(w, "Failed to encode career level", http.StatusInternalServerError)
+		return
+	}
+}
+func GetCareerLevelByID(w http.ResponseWriter, r *http.Request) {
+	careerLevelID := chi.URLParam(r, "id")
+
+	repo, err := db.NewCareerLevelRepository()
+	if err != nil {
+		http.Error(w, "Failed to connect to the database", http.StatusInternalServerError)
+		return
+	}
+	defer repo.Close()
+
+	careerLevel, err := repo.GetCareerLevelByID(careerLevelID)
+	if err != nil {
+		http.Error(w, "Failed to fetch career level", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err = json.NewEncoder(w).Encode(careerLevel)
+	if err != nil {
+		http.Error(w, "Failed to encode career level", http.StatusInternalServerError)
+		return
+	}
+}
+
+func CreateCareerLevel(w http.ResponseWriter, r *http.Request) {
+	var careerLevel models.CareerLevel
+
+	err := json.NewDecoder(r.Body).Decode(&careerLevel)
+	if err != nil {
+		http.Error(w, "Failed to decode career level data", http.StatusBadRequest)
+		return
+	}
+
+	repo, err := db.NewCareerLevelRepository()
+	if err != nil {
+		http.Error(w, "Failed to connect to the database", http.StatusInternalServerError)
+		return
+	}
+	defer repo.Close()
+
+	err = repo.CreateCareerLevel(careerLevel.Name)
+	if err != nil {
+		http.Error(w, "Failed to create career level", http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func DeleteCareerLevel(w http.ResponseWriter, r *http.Request) {
+	careerLevelID := chi.URLParam(r, "id")
+
+	repo, err := db.NewCareerLevelRepository()
+	if err != nil {
+		http.Error(w, "Failed to connect to the database", http.StatusInternalServerError)
+		return
+	}
+	defer repo.Close()
+
+	err = repo.DeleteCareerLevel(careerLevelID)
+	if err != nil {
+		http.Error(w, "Failed to delete career level", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func UpdateCareerLevel(w http.ResponseWriter, r *http.Request) {
+	careerLevelID := chi.URLParam(r, "id")
+	var careerLevel models.CareerLevel
+	err := json.NewDecoder(r.Body).Decode(&careerLevel)
+	if err != nil {
+		http.Error(w, "Failed to decode career level data", http.StatusBadRequest)
+		return
+	}
+
+	repo, err := db.NewCareerLevelRepository()
+	if err != nil {
+		http.Error(w, "Failed to connect to the database", http.StatusInternalServerError)
+		return
+	}
+	defer repo.Close()
+
+	err = repo.UpdateCareerLevel(careerLevelID, careerLevel.Name)
+	if err != nil {
+		http.Error(w, "Failed to update career level", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
